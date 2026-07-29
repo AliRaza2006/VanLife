c90a6c866895a668343eb6150489d0681dd1c561
AliRaza2006 <maliraza2019@gmail.com>
2026-07-29 15:05:30 +0500

    added styling on root and removed .appContainer

--- index.jsx
+++ index.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className='appContainer'>
+    <div>
       <BrowserRouter>
       <Routes>
       <Route path="/" element={<Home/>}/>
