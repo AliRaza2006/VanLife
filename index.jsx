be968dc348e237ffa730bd7ed76d049baa50fc2f
AliRaza2006 <maliraza2019@gmail.com>
2026-07-31 03:36:21 +0500

    added layout route

--- index.jsx
+++ index.jsx
@@ -4,24 +4,23 @@ import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Vans from './pages/Vans.jsx';
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
 import  "./server.js"
 import Van from './pages/Van.jsx';
+import Layout from './components/Layout.jsx';
 
 function App() {
   Van(1)
   return (
     <div>
       <BrowserRouter>
-        <Header/>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/vans" element={<Vans/>}/>
-          <Route path='/van/:id' element={<Van/>}/>
+          <Route element={<Layout/>}>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/vans" element={<Vans/>}/>
+            <Route path='/van/:id' element={<Van/>}/>
+          </Route>
         </Routes>
-        <Footer/>
       </BrowserRouter>
     </div>
   )
